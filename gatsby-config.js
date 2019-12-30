83c45ea88b63d92a96fe40e0619cd64065045a32
DamnedScholar <stickfigureonfire@gmail.com>
2019-12-30 13:09:26 -0600

    Prototyping

--- gatsby-config.js
+++ gatsby-config.js
@@ -113,7 +113,7 @@ module.exports = {
         setup(ref) {
           const ret = ref.query.site.siteMetadata.rssMetadata;
           ret.allMarkdownRemark = ref.query.allMarkdownRemark;
-          ret.generator = "GatsbyJS Advanced Starter";
+          ret.generator = "Holland Wilson's Personal Page";
           return ret;
         },
         query: `
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
+import "./foundation.min.css";
 import "./index.less";
 
 export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
+    const fontsURL = `https://fonts.googleapis.com/css?family=EB+Garamond|Sanchez|Shadows+Into+Light&display=swap`
+    const faURL = `https://kit.fontawesome.com/979ad25e6f.js`
+
     return (
       <div className="layout-container">
+        <div className="bg unsplash" />
         <Helmet>
           <meta name="description" content={config.siteDescription} />
           <html lang="en" />
+          <link href={fontsURL} rel="stylesheet" />
+          <script src={faURL} crossorigin="anonymous"></script>
         </Helmet>
         {children}
       </div>
